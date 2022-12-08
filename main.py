61b6e18f106e0c7610e2119a2efa95c6407d3476
oslohes123 <ashleytyagi@hotmail.com>
2022-12-08 14:35:04 +0000

    Updated solution which is more complete however with a worse time complexity

--- main.py
+++ main.py
@@ -5,11 +5,18 @@ c = [2, 8, 18, 2, 1, 1, 8]
 
 def solve(a, b, c):
     answer_set = list()
-    for i in range(0, len(a)):
-        input_tuple = sorted([a[i], b[i], c[i]])
-        answer_set.append(tuple(input_tuple))
+    answer_set.append((a[0], b[0], c[0]))
+    for i in range(1, len(a)):
+        input_tuple = [a[i], b[i], c[i]]
+        sorted_tuple = sorted([a[i], b[i], c[i]])
+        found = False
+        for items in answer_set:
+            if sorted(items) == sorted_tuple:
+                found = True
+                break
+        if not found:
+            answer_set.append(input_tuple)
 
-    answer_set = list(dict.fromkeys(answer_set))
     print(answer_set)
 
 
